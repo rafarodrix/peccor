f789f0253b174c79016b8ebf9810e3ebb30a60de
Rafael Rodrigues <rafaelrodrix@gmail.com>
2026-05-25 16:52:11 -0300

    integração banco de daos

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,37 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
+  prismaPool: Pool | undefined;
 };
+
+const connectionString = process.env.DIRECT_URL ?? process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL ou DIRECT_URL precisa estar definido");
+}
+
+const pool =
+  globalForPrisma.prismaPool ??
+  new Pool({
+    connectionString,
+  });
+
+const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+  globalForPrisma.prismaPool = pool;
+}
